ec52d502b0354fa344353361b51558e6e0027b2b
Farhan Alvi <farhanalvi005@gmail.com>
2023-12-04 02:37:02 +0400

    add comments

--- day1/main/main.go
+++ day1/main/main.go
@@ -18,17 +18,19 @@ func printer(name string) func(any) {
 }
 
 func main() {
-	const filepath = "big.txt"
+	const filepath = "input.txt"
+
+	// part 1
 	file, err := os.Open(filepath)
 	if err != nil {
 		panic(err)
 	}
-
 	printPart1 := printer("part1")
 	p1sum := day1.Part1(file)
 	printPart1(p1sum)
 	file.Close()
 
+	// part 2
 	file, err = os.Open(filepath)
 	if err != nil {
 		panic(err)
