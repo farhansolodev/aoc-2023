1d4b1b9ebbc342396130c1aad887df21f4260dc5
Farhan Alvi <farhanalvi005@gmail.com>
2023-12-06 04:20:35 +0400

    revert to old output

--- day1/main.go
+++ day1/main.go
@@ -9,10 +9,12 @@ import (
 	day1 "github.com/farhansolodev/aoc-2023/src"
 )
 
-func timer() func() time.Duration {
+func printer(name string) func(any) {
 	start := time.Now()
-	return func() time.Duration {
-		return time.Since(start)
+	return func(v any) {
+		duration := time.Since(start)
+		fmt.Printf("%s: %v\n", name, v)
+		fmt.Printf("-- took %v\n", duration)
 	}
 }
 
@@ -25,24 +27,18 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
-	getPart1Duration := timer()
+	printPart1 := printer("part1")
 	p1sum := day1.Part1(file)
-	p1d := getPart1Duration()
+	printPart1(p1sum)
 	file.Close()
-	fmt.Printf("%s: %v\n", "part1", p1sum)
-	fmt.Printf("-- took %v\n", p1d)
 
 	// part 2
 	file, err = os.Open(*filepath)
 	if err != nil {
 		panic(err)
 	}
-	getPart2Duration := timer()
+	printPart2 := printer("part2")
 	p2sum := day1.Part2(file)
-	p2d := getPart2Duration()
+	printPart2(p2sum)
 	file.Close()
-	fmt.Printf("%s: %v\n", "part2", p2sum)
-	fmt.Printf("-- took %v\n", p2d)
-
-	fmt.Printf("\nPart 2 took %fx longer", float32(p2d.Microseconds())/float32(p1d.Microseconds()))
 }
